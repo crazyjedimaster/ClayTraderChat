f6acbbc8b970356fc222313643a5f5e346105a12
crazyjedimaster <steven.vanmanen@gmail.com>
2015-03-07 18:05:42 -0600

    port change

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var app = require('express').createServer();
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 
-app.listen(8080);
+app.listen(server_port);
 //var d = new Date();
 var today = date();
 var mesHisRoom1 = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
